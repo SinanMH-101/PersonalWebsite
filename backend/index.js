59df8484afd6ed375794d1b947ad40aa59773a09
Sinan <sinanmhaque@gmail.com>
2025-10-08 20:39:21 +1100

    Updated build for render deployment

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express()
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'))
 
 await mongoose.connect(process.env.MONGODB_URI);
 
