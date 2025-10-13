2d1e61b96f86555a01f2623a1e313a29083b066a
Sinan Haque <sinanmhaque@gmail.com>
2025-10-14 10:51:58 +1100

    updated script and minor design tweaks

--- backend/scripts/projectSeed.mjs
+++ backend/scripts/projectSeed.mjs
@@ -4,16 +4,19 @@ import Project from "../models/projects.js"
 
 await mongoose.connect(process.env.MONGODB_URI, { dbName: 'projects' });
 await Project.deleteMany({});
+console.log("Deleted ✔");
 await Project.insertMany([
     {
         title: "Flashcard Study App",
         short_desc: "A Flashcard App Made using React + TypeScript with MobX and local persistence.",
         long_desc: `Built an interactive flashcard app with real-time flipping, deletion, and local persistence.
 Developed reusable, modular React components to render and manage dynamic flashcards.
-Used MobX for state management and TypeScript for type-safe component logic.`,
-        tech: ["React", "TypeScript", "MobX", "Vite", "HTML", "CSS"],
+Used MobX for state management and TypeScript for type-safe component logic.
+\nThis was one of my first frontend projects and so I was mainly focusing on learning how React works and decided to make something that I would atleast use from time to time.
+Pretty happy with the final outcome but I do plan to eventually sync it to a databse so that I can properly use it, but for now local persistence seemed to be sufficient.`,
+        tech: ["React", "TypeScript", "MobX", "Vite", "HTML"],
         link: "",
-        git: "",
+        git: "https://github.com/SinanMH-101/flashcard-study-app",
         imgPath: "assets/FlashCard.png",
         highlighted: true
     },
@@ -22,10 +25,13 @@ Used MobX for state management and TypeScript for type-safe component logic.`,
         short_desc: "EDA + regression/classification on global video game sales.",
         long_desc: `Conducted EDA with Pandas/NumPy to find genre preferences, regional trends, and rating correlations.
 Built and evaluated Linear, Polynomial, and Logistic Regression models to predict sales figures.
-Visualized trends and distributions with Matplotlib for clear insights.`,
+Visualized trends and distributions with Matplotlib for clear insights.
+\n Originally made as the final project for my Data Science (COMP2300) class. This was my first time really delving into Data Science
+ and I learned a lot. The project can also be further improved with a software such as PowerBI, but the asessment required us to do it
+  all on a Jupyter notebook, hence all the diagrams made with the aformentioned libraries in Python. Pretty satisfied with the projects and happy that I ended up with a HD`,
         tech: ["Python", "Pandas", "NumPy", "Matplotlib", "Scikit-learn"],
         link: "",
-        git: "",
+        git: "https://github.com/SinanMH-101/video-game_data_analysis",
         imgPath: "",
         highlighted: false
     },
@@ -37,7 +43,7 @@ Implemented Firebase Auth for secure sign-in; Firestore for history/stats; Stora
 Added customizable home screen via SharedPreferences; wrote unit/widget tests with Mockito.`,
         tech: ["Flutter", "Firebase (Auth, Firestore, Storage)", "SharedPreferences", "Mockito"],
         link: "",
-        git: "",
+        git: "https://github.com/SinanMH-101/CleanWalk",
         imgPath: "",
         highlighted: true
     },
@@ -47,9 +53,9 @@ Added customizable home screen via SharedPreferences; wrote unit/widget tests wi
         long_desc: `Built a desktop banking application with a Java Swing GUI to manage multiple user accounts.
 Enabled account selection via dropdown with real-time balance updates, deposits, and withdrawals.
 Applied object-oriented design to encapsulate account logic and practice event-driven programming.`,
-        tech: ["Java", "Swing", "OOP", "GUI"],
+        tech: ["Java", "Swing"],
         link: "",
-        git: "",
+        git: "https://github.com/SinanMH-101/bank_app",
         imgPath: "",
         highlighted: false
     },
@@ -61,9 +67,9 @@ Server accepts multiple client connections and broadcasts messages in real time.
 Added nicknames, clean terminal formatting, and graceful disconnect/error handling.
 Practiced TCP/IP basics, client vs. server logic, concurrency, and debugging dropped connections & encoding issues.
 How to run: start server with "python server.py", then connect clients with "python client.py".`,
-        tech: ["Python 3", "socket", "threading", "TCP/IP", "CLI/Terminal"],
+        tech: ["Python", "TCP/IP"],
         link: "",
-        git: "",
+        git: "https://github.com/SinanMH-101/simple_chat_room",
         imgPath: "",
         highlighted: false
     },
@@ -75,7 +81,7 @@ Features include a color-coded transaction list, one-click deletion, responsive 
 Developed as a practice project to strengthen React, TypeScript, and MobX skills.`,
         tech: ["React", "TypeScript", "MobX", "Vite", "CSS", "LocalStorage"],
         link: "",
-        git: "",
+        git: "https://github.com/SinanMH-101/personal_finance_tracker",
         imgPath: "",
         highlighted: true
     }
